8f419eafd8534d7c1785087d40fc75a8c118ea4b
Sandip Rijal <sandurijal03@hotmail.com>
2024-01-28 19:18:00 +0545

    server: wrapping express server with http and added some middleware for cors and posting data on express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,28 @@
+import http from 'http'
+
 import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
 import connectDB from './db/connectDB'
-import dotenv from "dotenv"
-
-
 
 const mountServer = async () => {
   const app = express()
   dotenv.config()
   await connectDB()
 
+  // middleware
+  app.use(express.json())
+  app.use(cors())
+
+  const server = http.createServer(app)
+
   app.get('/', (req: Request, res: Response) => {
     res.json({ message: 'started' })
   })
 
   const port = process.env.PORT || 5000
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log('server: mounted at port ', port)
   })
 }
