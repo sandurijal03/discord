37d45e3422804c8e5ce071e3e61b658ac2cd688f
Sandip Rijal <sandurijal03@hotmail.com>
2024-01-29 19:43:27 +0545

    server: created logic for registering user

--- backend/src/routes/auth/index.controller.ts
+++ backend/src/routes/auth/index.controller.ts
@@ -1,6 +1,47 @@
 import { Request, Response } from 'express'
+import bcrypt from 'bcryptjs'
+
+import User from '../../models/User'
 
 export const loginController = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body
+
+    // check if user exists
+    const userExists = await User.exists({ email })
+
+    if (userExists) {
+      return res.status(409).json({
+        success: false,
+        message: 'Email is already in use. Please try again with another email',
+      })
+    }
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    })
+
+    // create jwt token
+    const token = 'jwt token'
+
+    return res.status(201).json({
+      success: true,
+      message: 'User Registered Successfully',
+      userDetails: {
+        email: user.email,
+        token,
+        username: user.username,
+      },
+    })
+  } catch (err) {
+    console.log('err', err)
+    return res
+      .status(500)
+      .json({ success: false, message: 'Error occured. Please try again' })
+  }
   res.json({ message: 'login' })
 }
 
