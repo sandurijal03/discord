d56bc7d1eee06db377a47b60dfc1d17284bceeb5
Sandip Rijal <sandurijal03@hotmail.com>
2024-02-01 15:35:20 +0545

    client: created login page header

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,25 @@
 import * as React from 'react'
+import { Typography } from '@mui/material'
+import AuthBox from '../shared/components/AuthBox'
+
+const LoginPageHeader = () => {
+  return (
+    <>
+      <Typography variant='h5' sx={{ color: '#fff' }}>
+        Welcome Back!
+      </Typography>
+      <Typography sx={{ color: '#b9bbbe' }}>
+        We are happy that you are with us!
+      </Typography>
+    </>
+  )
+}
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <AuthBox>
+      <LoginPageHeader />
+    </AuthBox>
   )
 }
 
